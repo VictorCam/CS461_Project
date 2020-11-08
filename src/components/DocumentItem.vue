7d45e36fb47ab5e2aa084026eea60bfdf650dae1
Bryce Albertazzi <brycealbertazzi@gmail.com>
2020-11-07 20:57:49 -0800

    Barebones UI
    Will check with group to make sure I'm on the right track.

---
+++ src/components/DocumentItem.vue
@@ -0,0 +1,16 @@
+<template>
+   <div class="document-item">
+       <p>{{doc.content}}</p>
+    </div> 
+</template>
+
+<script>
+export default {
+    name: "DocumentItem",
+    props: ["doc"]
+}
+</script>
+
+<style scoped>
+
+</style>
