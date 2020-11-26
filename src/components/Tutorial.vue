d99a8b425cd8cdcc10789292151ebd4cae492aec
brycealbertazzi <yourgitemail@example.com>
2020-11-25 17:48:19 -0800

    Added images for the tutorial steps

--- src/components/Tutorial.vue
+++ src/components/Tutorial.vue
@@ -4,18 +4,24 @@
     <div class="content">
         <div class="steps">
             <h2>
-                1. Send an email to go beavdms@gmail.com
+                1. Open an email message, include gobeavdms@gmail.com as the recipient
             </h2>
+            <img src="https://www.spcdn.org/images/undisclosed-recipients-gmail.jpg" alt="">
         </div>
         <div class="steps">
             <h2>
-                2. Include content in the header of the email
+                2. Include one of these keywords in the subject: HELP, SAVE, GET
+                <h5>HELP: Get further information from the documentation</h5>
+                <h5>SAVE: Saves a composed email to the file system</h5>
+                <h5>GET: Retrieves a document from the file system</h5>
             </h2>
+            <img src="https://www.wikihow.com/images/thumb/c/cf/Send-an-Email-Using-Gmail-Step-12-Version-2.jpg/v4-460px-Send-an-Email-Using-Gmail-Step-12-Version-2.jpg.webp" alt="">
         </div>
         <div class="steps">
             <h2>
                 3. Optionally include any desired attachments
             </h2>
+            <img src="https://acflory.files.wordpress.com/2015/07/picture-file-attach-icon.jpg" alt="">
         </div>
         <div class="steps">
             <h2>
@@ -44,6 +50,8 @@ export default {
 }
 .steps {
     align-self: flex-start;
-    margin: 30px;
+    margin-left: 40px;
+    margin-right: 40px;
+    margin-bottom: 100px;
 }
 </style>
