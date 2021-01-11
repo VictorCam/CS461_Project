19a37eea79afaed6dd1adcba098969157769152c
brycealbertazzi <yourgitemail@example.com>
2021-01-10 23:39:52 -0800

    Added BootstrapVue

---
+++ src/components/layout/SearchSort 3.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="search-sort">
+        <div class="search-bar">
+            <input type="text" placeholder="Search...">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SearchSort"
+};
+</script>
+
+<style scoped>
+    .search-sort {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 20px;
+        background-color: #ddd;
+        flex-wrap: wrap;
+    }
+    
+    .search-bar {
+        position: relative;
+    }
+    
+</style>
---
+++ src/components/layout/SearchSort.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="search-sort">
+    <div class="search-bar">
+      <b-input-group size="lg" prepend="Search">
+        <b-form-input></b-form-input>
+      </b-input-group>
+    </div>
+    <div class="sort-btn">
+      <b-button variant="outline-secondary" size="lg">Sort</b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchSort",
+};
+</script>
+
+<style scoped>
+.search-sort {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+  background-color: #ddd;
+  flex-wrap: wrap;
+}
+
+.sort-btn {
+  position: absolute;
+  padding: 2px;
+  margin-left: 90%;
+}
+</style>
