22163db190820b09fcc15298ebad175c9500e43d
brycealbertazzi <yourgitemail@example.com>
2021-01-27 14:36:45 -0800

    Added search bar modal, updated tutorial page

--- src/components/DocumentItem.vue
+++ src/components/DocumentItem.vue
@@ -19,7 +19,6 @@
                 </b-button>
             </router-link>
         </div>
-       
     </div> 
 </template>
 
--- src/components/layout/SearchSort.vue
+++ src/components/layout/SearchSort.vue
@@ -9,9 +9,32 @@
       </b-input-group>
     </div>
     <div class="sort-btn">
-      <b-button variant="secondary" size="lg">
+      <b-button variant="secondary" size="lg" v-b-modal.sort-modal>
         <b-icon-sort-down-alt></b-icon-sort-down-alt>
       </b-button>
+      <b-modal id="sort-modal" title="Sort Documents">
+        <div>
+          <b-form-group label="Sort By:" v-slot="{ ariaDescribedby }">
+            <b-form-radio-group
+              v-model="selected"
+              :options="options"
+              :aria-describedby="ariaDescribedby"
+              name="radios-stacked"
+              size="lg"
+              stacked
+            >
+
+              <b-form-radio value="Sort By Date" v-model="selected" :aria-describedby="ariaDescribedby" name="sort-radio" class="my-1">Date Added</b-form-radio>
+              <b-form-radio value="Sort By Document Name" v-model="selected" :aria-describedby="ariaDescribedby" name="sort-radio" class="my-1">Document Name Alphabetical</b-form-radio>
+              <b-form-radio value="Sort By Project Name" v-model="selected" :aria-describedby="ariaDescribedby" name="sort-radio" class="my-1">Project Name Alphabetical</b-form-radio>
+
+            </b-form-radio-group>
+          </b-form-group>
+
+          <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
+          
+        </div>
+      </b-modal>
     </div>
   </div>
 </template>
@@ -29,6 +52,14 @@ export default {
       } else {
         this.$store.dispatch("search_documents", this.searchText);
       }
+    }
+  },
+  data() {
+    return {
+      selected: '',
+      options: [
+    
+      ]
     }
   }
 };
